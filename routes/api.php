3adeb95f51286c94f78d973c646099409929845b
Shifatullah Rizwan <5946849+Shifatullah@users.noreply.github.com>
2018-07-10 21:47:33 +1000

    Laravel products api project initial commit

---
+++ routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('v1/products', function() {
+    // If the Content-Type and Accept headers are set to 'application/json',
+    // this will return a JSON structure. This will be cleaned up later.
+    return collect(["iPhone X", "Samsung 9"])->all();;
+});
